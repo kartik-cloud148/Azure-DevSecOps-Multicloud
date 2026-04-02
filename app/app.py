e396ce2f608d2ace4c8a7bb37784a9a6f4f74ef8
Ubuntu <azureuser@bastion-vm.ml0urnt4bz4ufft3ugfkg1omwf.rx.internal.cloudapp.net>
2026-04-02 09:31:01 +0000

    added service and deployment files

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevSecOps Brain Running 🚀"
+    return "DevSecOps Brain Running 🚀 and Argo cd is working as well "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
